3c06751f4416cd0269f37221bc6bcfc20efcf23d
N0mercy00 <dkdhehesp@naver.com>
2021-01-07 17:08:14 +0900

    CSS2

--- App.js
+++ App.js
@@ -1,30 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.first} >
-        <Text>im first</Text>    
-      </View>
-      <View style={styles.second}>
-           <Text>me second</Text>
-      </View>
-    </View>
+   <Loading></Loading>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  first:{
-    flex:1,
-    backgroundColor:"red"
-  },
-  second:{
-    flex:3,
-    backgroundColor:"orange"
-  }
-});
+
