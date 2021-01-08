155bd390618d48cf3aed7ec5dc69f933aa327db7
N0mercy00 <dkdhehesp@naver.com>
2021-01-08 19:56:46 +0900

    almost done

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Alert} from "react-native";
 import Loading from "./Loading";
@@ -6,13 +5,11 @@ import * as Location from 'expo-location';
 import axios from "axios";
 import Weather from "./Weather";
 
-
 const API_KEY="";
-
-
+var condition="";
 export default class extends React.Component {
   state={
-    isLoading:true
+    isLoading:true 
   };
   getWeather = async(latitude,longitude)=>{
     const {
@@ -23,35 +20,39 @@ export default class extends React.Component {
     } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
       );
-    
       this.setState({ 
         isLoading:false,
         condition : weather[0].main ,
-        temp
-        
+        temp   
       });
-      
+  
+     console.log(temp);
+     condition=weather[0].main;
+     console.log(condition);   
   };
   getLocation=async()=>{
     try {
       await Location.requestPermissionsAsync();
-      const {coords:{latitude,longitude}
+      const {
+        coords:{latitude,longitude}
     }= await Location.getCurrentPositionAsync();
-
     this.getWeather(latitude,longitude);
-    this.setState({isLoading:false});
-     //Send to API and Get weather
+    
+     
     } catch (error) {
       Alert.alert("Can't find tou","So sad :(");
     }
     
-  }
+  };
   componentDidMount(){
     this.getLocation();
   }
   render(){
     const {isLoading,temp,condition} = this.state;
-    return(isLoading?<Loading/>:<Weather temp={Math.round(temp)} condition={condition}/>
+    return isLoading?(
+    <Loading/>
+    ):(
+    <Weather temp={Math.round(temp)} condition={condition}/>
       );
   }
 }
--- Weather.js
+++ Weather.js
@@ -1,18 +1,63 @@
 import React from 'react';
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, StatusBar} from "react-native";
 import PropTypes from "prop-types";
 import {MaterialCommunityIcons} from "@expo/vector-icons";
 
-export default function Weather({temp}){
-    return (<View style={styles.container}>
-            <View style={styles.halfcontainer}>
-                <MaterialCommunityIcons size={96} name="weather-lightning-rainy"/>
-                <Text style={styles.temp}>{temp}</Text>
-            </View>
-            <View style={styles.halfcontainer}>
+import { LinearGradient } from 'expo-linear-gradient';
 
-            </View>
-        </View>);
+const weatherOptions={
+    Haze:{
+        iconName:"weather-hail",
+        gradient:["#4DA0B0","#D39D38"]
+    },
+    Clouds:{
+        iconName:"weather-cloudy",
+        gradient:["#8360c3","#2ebf91"]
+    },
+    Thunderstorm:{
+        iconName:"weather-lightning",
+        gradient:["#544a7d","#ffd452"]
+    },
+   
+    Rain: {
+        iconName:"weather-rainy",
+        gradient:["#00B4DB","#0083B0"]
+    },
+    Snow: {
+        iconName:"weather-snowy",
+        gradient:["#FFEFBA","#FFFFFF"]
+    },
+    
+    Clear: {
+        iconName:"weather-sunny",
+        gradient:["#4AC29A","#BDFFF3"]
+    },
+    Dust: {
+        iconName:"warning",
+        gradient:["#EB5757","#000000"]
+    },
+    
+};
+
+export default function Weather({temp,condition}) {
+    return (
+       
+            <LinearGradient
+                // Background Linear Gradient
+                colors={weatherOptions[condition].gradient} style={styles.container}>
+                <StatusBar barStyle="light-content"/>
+                <View style={styles.halfcontainer}>
+                    <MaterialCommunityIcons 
+                    size={96} 
+                    name={weatherOptions[condition].iconName} //icon
+                    color="white"/>
+                    <Text style={styles.textcondition}>Today is {condition}</Text> 
+                    <Text style={styles.temp}>{temp}℃</Text>
+                </View>
+                <View style={styles.halfcontainer}></View>
+            </LinearGradient>
+      
+    );
 }
 
 Weather.propTypes={
@@ -38,11 +83,15 @@ const styles =StyleSheet.create({
         alignItems:"center"
     },
     temp:{
-        fontSize:42
+        fontSize:42,
+        color:"white"
     },
     halfcontainer:{
         flex:1,
         justifyContent:"center",
         alignItems:"center"   
+    },textcondition:{
+        fontSize:20,
+        color:"white"
     }
 });
