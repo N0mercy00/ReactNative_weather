3922caa978ee6c24b44f35132e0919d2202a5512
N0mercy00 <dkdhehesp@naver.com>
2021-01-08 15:28:05 +0900

    layout

--- Weather.js
+++ Weather.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import {View, Text, StyleSheet} from "react-native";
 import PropTypes from "prop-types";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 export default function Weather({temp}){
-    return <View style={styles.container}>
-        <Text>{temp}</Text>
-        </View>
+    return (<View style={styles.container}>
+            <View style={styles.halfcontainer}>
+                <MaterialCommunityIcons size={96} name="weather-lightning-rainy"/>
+                <Text style={styles.temp}>{temp}</Text>
+            </View>
+            <View style={styles.halfcontainer}>
+
+            </View>
+        </View>);
 }
 
-Weather.PropTypes={
-    temp:PropTypes.number.isRequired
-}
+Weather.propTypes={
+    temp:PropTypes.number.isRequired,
+    condition:PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Dust",
+        "Mist"
+    ]).isRequired
+};
 
 const styles =StyleSheet.create({
     container:{
         flex:1,
         justifyContent:"center",
         alignItems:"center"
+    },
+    temp:{
+        fontSize:42
+    },
+    halfcontainer:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"   
     }
 });
