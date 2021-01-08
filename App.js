5d3244e0d1dbcd394353a0ac28463041448db760
N0mercy00 <dkdhehesp@naver.com>
2021-01-08 12:51:48 +0900

    get temp

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ import {Alert} from "react-native";
 import Loading from "./Loading";
 import * as Location from 'expo-location';
 import axios from "axios";
+import Weather from "./Weather";
 
 
 const API_KEY="";
+
 
 export default class extends React.Component {
   state={
@@ -14,10 +16,12 @@ export default class extends React.Component {
   };
   getWeather = async(latitude,longitude)=>{
     const {data} = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
       );
       console.log(data);
-  }
+      console.log(data.main.temp);
+      this.setState({ isLoading:false, temp:data.main.temp});
+  };
   getLocation=async()=>{
     try {
       await Location.requestPermissionsAsync();
@@ -36,8 +40,8 @@ export default class extends React.Component {
     this.getLocation();
   }
   render(){
-    const {isLoading} = this.state;
-    return(isLoading?<Loading/>:null
+    const {isLoading,temp} = this.state;
+    return(isLoading?<Loading/>:<Weather temp={Math.round(temp)}/>
       );
   }
 }
