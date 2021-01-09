07698466bbc2a7b360ad38e18d23018e655d4397
N0mercy00 <dkdhehesp@naver.com>
2021-01-09 11:16:01 +0900

    finsish!!

--- Weather.js
+++ Weather.js
@@ -8,33 +8,55 @@ import { LinearGradient } from 'expo-linear-gradient';
 const weatherOptions={
     Haze:{
         iconName:"weather-hail",
-        gradient:["#4DA0B0","#D39D38"]
+        gradient:["#4DA0B0","#D39D38"],
+        title:"Be careful in front",
+        subtitle:"It's better to look ahead today.\n"+
+        "Be careful driving in the morning"
     },
     Clouds:{
         iconName:"weather-cloudy",
-        gradient:["#8360c3","#2ebf91"]
+        gradient:["#8360c3","#2ebf91"],
+        title:"I like this day",
+        subtitle:"Did you see the sky?\n"+
+        "it's a dreamy cloud"
     },
     Thunderstorm:{
         iconName:"weather-lightning",
-        gradient:["#544a7d","#ffd452"]
+        gradient:["#544a7d","#ffd452"],
+        title:"Be careful!!",
+        subtitle:"Thor is angry\n"+
+        "He must have had a hard day (OoO)"
     },
    
     Rain: {
         iconName:"weather-rainy",
-        gradient:["#00B4DB","#0083B0"]
+        gradient:["#00B4DB","#0083B0"],
+        title:"Don't forget the umbrella",
+        subtitle:"if you want me to meet you\n"+
+        "you can leave your umbrella\n"+
+        "i will go there for you"
     },
     Snow: {
         iconName:"weather-snowy",
-        gradient:["#FFEFBA","#FFFFFF"]
+        gradient:["#C4E0E5","#4CA1AF"],
+        title:"Be careful especially like today",
+        subtitle:"if you slips\n"+
+        "You will be very painful"
     },
     
     Clear: {
         iconName:"weather-sunny",
-        gradient:["#4AC29A","#BDFFF3"]
+        gradient:["#00d2ff","#928DAB"],
+        title:"Good morning",
+        subtitle:"Did you look out the window?\n"+
+        "The weather is so good today"
     },
     Dust: {
         iconName:"weather-cloudy-alert",
-        gradient:["#EB5757","#000000"]
+        gradient:["#EB5757","#000000"],
+        title:"Oh God",
+        subtitle:"i really hate 'that' country\n"+
+        "Including Wuhan virus ༼ つ ◕_◕ ༽つ"
     },
     
 };
@@ -54,7 +76,10 @@ export default function Weather({temp,condition}) {
                     <Text style={styles.textcondition}>Today is {condition}</Text> 
                     <Text style={styles.temp}>{temp}℃</Text>
                 </View>
-                <View style={styles.halfcontainer}></View>
+                <View style={styles.under}>
+                    <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                    <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
+                </View>
             </LinearGradient>
       
     );
@@ -81,17 +106,38 @@ const styles =StyleSheet.create({
         flex:1,
         justifyContent:"center",
         alignItems:"center"
+        
     },
     temp:{
         fontSize:42,
         color:"white"
     },
     halfcontainer:{
-        flex:1,
+        flex:2,
         justifyContent:"center",
         alignItems:"center"   
-    },textcondition:{
+    },
+    under:{
+        flex:3,
+        justifyContent:"center",
+        alignItems:"center"  
+    },
+    textcondition:{
         fontSize:20,
         color:"white"
+       
+    },
+    title:{
+        color:"white",
+        fontSize:30,
+        fontWeight:"100",
+        marginBottom:10,
+        marginTop:-20
+    },
+    subtitle:{
+        fontWeight:"700",
+        color:"white",
+        fontSize:20,
+        margin:10
     }
 });
